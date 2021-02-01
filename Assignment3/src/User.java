ead984e1cc9257f572aa4b05d3d4234255a5f1fe
Filip Kulevski <filipkule@gmail.com>
2021-02-01 22:46:18 +0100

    Code and program flow refactor.

--- Assignment3/src/User.java
+++ Assignment3/src/User.java
@@ -1,9 +1,9 @@
 
 public class User {
 
-	String username;
-	String password;
-	String fullName;
+	private String username;
+	private String password;
+	private String fullName;
 	
 	public String getUsername() {
 		return username;
